730ab4b03e254065ee19991d1acb5f4ccbd6e526
rjrbreikss <hamste126@gmail.com>
2014-05-02 01:13:03 +0300

    Refactoring and adding comments

--- library.cpp
+++ library.cpp
@@ -3,11 +3,11 @@
 #include <stdio_ext.h>
 #include <stdlib.h>
 
+#include <time.h>
+
 #include <stdarg.h>
 
 #include "Menu.cpp"
-
-#define member_size(type, member) sizeof(((type *)0)->member)
 
 /*
 --------------
@@ -37,44 +37,46 @@ Uzdevums: Atrast visas kada autora gramatas (autoru ievada lietotajs)
 /**
 *	Structure type definitions
 */
-struct book_rec_type{ 							//  Struktura ar informaciju par gramatam
-    unsigned short int 	id;
-    char 			    title[20];
-    char 			    author[15];
-    char 			    publishing_house[20];
-    unsigned short int  year;
+struct book_rec_type{ 							// Struktura ar informaciju par gramatam
+    unsigned short int 	id;                     // Ieraksta ID
+    char 			    title[20];              // Nosaukums
+    char 			    author[15];             // Autors
+    char 			    publishing_house[20];   // Izdēvniecība
+    unsigned short int  year;                   // Izlaiduma gads
 };
 
-unsigned short int	    g_count; 					// Ierakstu skaits
+unsigned short int	    g_count; 				// Ierakstu skaits
 FILE			       *g_file;					// Fails
-book_rec_type 	       *g_book_rec_pt;				// Norade uz strukturu ( datubazes veidosanai )
-unsigned short int      g_max_id = 0;
+book_rec_type 	       *g_book_rec_pt;			// Pointeris uz masīvu no struktūŗas
+unsigned short int      g_max_id = 0;           // ID sekvence
 
-bool                    C_DEBUG = false;
-FILE                   *C_LOG_FILE = fopen("library.log", "a+");
+bool                    C_DEBUG = false;        // Debugot vai nē
 /**
 *	Function Header declarations
 */
-int ShowAllRecords( void ); 						// Apskatam DB
-int AddRec( void ); 						// Pievienoshanas funkcija
-int DelRec( void ); 						// Dzeshanas funkcija
-int FindRec( void ); 						// Atlasa pa 1
-int individ ( void ); 						// Individualaa uzdevuma funkcija
-int SaveDB( void ); 						// Saglabashanas funkcija
+int ShowAllRecords( void );                     // Apskatam DB
+int AddRec( void );                             // Pievienoshanas funkcija
+int DelRec( void );                             // Dzeshanas funkcija
+int FindRec( void );                            // Atlasa pa 1
+int individ ( void );                           // Individualaa uzdevuma funkcija
+int SaveDB( void );                             // Saglabashanas funkcija
 
 int ReadDB( void );
 
-int printline(const char *format, ...);
-int debug(const char *format, ...);
-int ERR_MEM_ALLOC( void );
-int askForConfirmation();
-int removeRecord(int indexToRemove);
-int searchElement( size_t offset, /* size_t memb_size,*/ int *target, const char* p_type );
+int printline(const char *format, ...);         // Izvade ar newline beigās
+int debug(const char *format, ...);             // Debugošanas procedūŗa
+int ERR_MEM_ALLOC( void );                      // Procedūra atmiņas iedalīšanas kļūdas gadījumam
+int askForConfirmation();                       // Darbības aptsiprināšanas procedūra
+int removeRecord(int indexToRemove);            // Ieraksta dzēšana no masīva
+
+                                                // Ieraksta meklēšana masīvā
+int searchElement( size_t offset, int *target, const char* p_type );
 /**
 	Main function
 */
 int main( int argc, char** argv ) {
 
+    // Vai slēgt debugošanu
     for (int i = 0; i < argc; ++i) {
         if(strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0)
             C_DEBUG = true;
@@ -82,35 +84,35 @@ int main( int argc, char** argv ) {
 
     debug("START");
 
+    // Atveram failu papildināšanai (lai izveidotu failu, ja tas neeksistē) binārajā režīmā
     g_file = fopen( "db.dat", "a+b" );
+
     fread( &g_count, sizeof( g_count ), 1, g_file); // Nolasa ierakstu daudzumu
 
-    fread( &g_max_id, sizeof( g_max_id ), 1, g_file); // Nolasa maks. id
+    fread( &g_max_id, sizeof( g_max_id ), 1, g_file); // Nolasa id sekvenci
 
-    debug("counter %hu", g_count);
+    debug("Records count %hu", g_count);
 
     if(g_count > 0){
         debug("main calloc");
+
+        // Iedalam atmiņu visiem ierakstiem, kurus satur fails
         g_book_rec_pt = ( struct book_rec_type * ) calloc(g_count, sizeof( struct book_rec_type ));
 
         if ( ( g_book_rec_pt == NULL )) {
-            return ERR_MEM_ALLOC();
+            exit(ERR_MEM_ALLOC());
         }
 
-    	{
-    		int i; // Cikla iterators
-    		
-            debug("iterate records");
-
-    		fread( g_book_rec_pt, sizeof(struct book_rec_type), g_count, g_file); // Tiek nolasiti ieraksti no faila
-            debug("read in records",g_count);            
-    	}
+        //Ielasam ierakstus masīvā    	
+    	fread( g_book_rec_pt, sizeof(struct book_rec_type), g_count, g_file); // Tiek nolasiti ieraksti no faila
+        debug("read in records",g_count);            
     }
-    fclose(g_file);
+    fclose(g_file); // Aizveram failu
 
     debug("Size of array %d", sizeof(*g_book_rec_pt));
 
 	{
+        // Inicializējam galveno izvēlni izmantojo Menu.h
 		Menu *MyMenu = new Menu;
 		struct MenuLine MenuLines[] = {
 			{"View All Records",&ShowAllRecords},
@@ -120,24 +122,25 @@ int main( int argc, char** argv ) {
 			{"Do something",&individ},
 			{"Save Changes",&SaveDB}
 		};
-		if(MyMenu->setLines( MenuLines, sizeof( MenuLines ) / sizeof( struct MenuLine ) ) == 0)
-			MyMenu->getMenu();
+		if(MyMenu->setLines( MenuLines, sizeof( MenuLines ) / sizeof( struct MenuLine ) ) == 0){
+			MyMenu->getMenu(); // Izvadam izvēlni
+        }
 		else
 			return EXIT_FAILURE;
 	}    
-    free( g_book_rec_pt );
+    free( g_book_rec_pt ); //Atbrīvojam masīva atmiņu
 	return EXIT_SUCCESS;
 }
 
 /* Datubazes ReadDB */
 int ShowAllRecords( void ){
-    ReadDB();
+    ReadDB(); // Izvadam visus ierakstus
     getchar();
     return EXIT_SUCCESS;
 }
 
 int ReadDB( void ) {
-    int i; /* Cikls */
+    int i; // Cikla skaitītājs
 
     printf("ID");
     printf("\tNosaukums");
@@ -145,7 +148,7 @@ int ReadDB( void ) {
     printf("\tIzdevnieciba ");
     printf("\tGads\n");
     printf("-------------------------------------------------------------------\n");
-    for (i = 0; i < g_count; i++) { /* Skatamies DB saturu */
+    for (i = 0; i < g_count; i++) { // Izvadam ieraksta datus
         printf("%hd", g_book_rec_pt[i].id);
         printf("\t%s", g_book_rec_pt[i].title);
         printf("\t%s", g_book_rec_pt[i].author);
@@ -160,11 +163,9 @@ int ReadDB( void ) {
 /* Funkcija ierakstu pievienosanai */
 int AddRec(void) {    
     book_rec_type tmp_rec;
-
-    system("clear");
-
     debug("adding records");
-    
+
+    // Aizpildam ieraksta datus
     printf("Nosaukums: ");
     scanf("%s",  tmp_rec.title);
     __fpurge(stdin);
@@ -181,68 +182,63 @@ int AddRec(void) {
         __fpurge(stdin);
     } while (tmp_rec.year < 1);    
 
-    if (g_count == 0) { // Ja ir tuksha, tad vnk ieraksta
+    if (g_count == 0) {
         debug("adding first record");
-        debug("size of array", sizeof(*g_book_rec_pt));
+        debug("size of array", sizeof(*g_book_rec_pt));        
         g_book_rec_pt = (book_rec_type *) (calloc(++g_count, sizeof(book_rec_type))); //Iedodam vietu pirmajam ierakstam
     } 
-    else // Ja nav, tad sakarto alfabeta seciba
+    else
     {   
+        // Iedalam vietu jaunajam elementam
         g_book_rec_pt = ( struct book_rec_type * ) realloc(g_book_rec_pt, (++g_count) * sizeof( struct book_rec_type)); // iedalam papildus atmiņu jaunajkam ierakstam
         if ((g_book_rec_pt == NULL)) {
-            return ERR_MEM_ALLOC();
+            exit(ERR_MEM_ALLOC());
         }           
     }    
-    g_book_rec_pt[g_count-1] = tmp_rec;
-    g_book_rec_pt[g_count-1].id = ++g_max_id;
+    g_book_rec_pt[g_count-1] = tmp_rec; // piesaistam jauno ierakstu masīvam
+    g_book_rec_pt[g_count-1].id = ++g_max_id; // Inkrementējam ID sekvenci un piesaistam id laukam
 
     return EXIT_SUCCESS;
 }
 
 /* Funkcija ierakstu dzesanai */
-int DelRec(void) {
-    int i; /* Cikls */
-    short * id; /* Dinamiskais masivs ar ID */
-    short error; // Parbauda vai tika ievadits pareizs ID
-    short tmp, tmp2;    
-    int  inputId;
+int DelRec(void) {        
+    unsigned short int  inputId; // Lietotāja inputs
 
-    system("clear");
-
-    ReadDB();
+    ReadDB(); // Izvadam visus ierakstus
 
     printline("Enter ID of book you want to delete:"); 
-    scanf("%d",&inputId);
+    scanf("%hu",&inputId);
 	__fpurge(stdin);
     {
         int my_rec, v_yes_no;;
         
         while(
+            //Ejam cauri visiem atrastajiem ierakstiem (bet vajadzētu būt tikai vienam)
             (my_rec = 
                 searchElement(  offsetof( struct book_rec_type, id)
                                 //, member_size( struct book_rec_type, id)
                                 , (int*)&inputId, 
                                 "USINT" )
             ) >= 0){
+                // Prasam vai dzēst ierakstu
                 printline("Are You sure You want to delete this record? [Y/n] ");
                 v_yes_no = askForConfirmation();                
                 if(v_yes_no == 1)
+                    //Dzēšam ierakstu
                     if(removeRecord(my_rec) == EXIT_SUCCESS)
                         printline("The record has been successfully removed");
         }
     }
+
     getchar();
     return EXIT_SUCCESS;
 }
 /* Atlasa pa vienam */
 int FindRec(void) {
-    int i; // Cikls   
-    unsigned short v_found = 0;
     char inputName[15]; // Lietotaja inputs
 
-    system("clear");
-
-    ReadDB();
+    ReadDB(); // Izvadam ierakstus
 
     printline("Ievadiet gramatas nosaukumu: ");
     scanf("%s",  inputName);
@@ -251,6 +247,7 @@ int FindRec(void) {
     {
         int my_rec;
         while(
+            // Ejam cauri visiem atrastajiem ierakstiem
             (my_rec = 
                 searchElement(  offsetof( struct book_rec_type, title)
                                 //, member_size( struct book_rec_type, title)
@@ -264,13 +261,13 @@ int FindRec(void) {
     return EXIT_SUCCESS;
 }
 
-/* Individualais uzdevums - konkretai atrashanai */
+/**
+    Individualais uzdevums - konkretai atrashanai pēc autora 
+*/
 int individ(void) {    
-    char inputName[20]; // Izdevn Nosauk salidzinashanai
+    char inputName[20]; //Salīdzināmā vērtīBa
 
-    system("clear");
-
-    ReadDB();
+    ReadDB(); // Izvadam ierakstus
 
     printline("Ievadiet gramatas autoru: ");    
     scanf("%s",  inputName);
@@ -279,6 +276,7 @@ int individ(void) {
     {
         int my_rec;
         while(
+            // Meklējam ierakstus, kamēr neatgriež negatīvu skaitli
             (my_rec = 
                 searchElement(  offsetof( struct book_rec_type, author)
                                 //, member_size( struct book_rec_type, author)
@@ -294,18 +292,17 @@ int individ(void) {
 
 /* Funkcija datu saglabasanai */
 int SaveDB(void) {
-    int i; // Cikls    
 
-    g_file = fopen("db.dat", "wb");
-    fwrite( & g_count, sizeof(g_count), 1, g_file); /* Jauno elementu skaitu saglabajam faila */
-
+    g_file = fopen("db.dat", "wb"); // Atveram failu rakstīšanai binārajā režīmā
+    fwrite( & g_count, sizeof(g_count), 1, g_file); // Jauno elementu skaitu saglabajam faila
     debug("G_COUNT: %d", g_count);
 
-    fwrite( &g_max_id, sizeof(g_max_id), 1, g_file); /* Jauno elementu skaitu saglabajam faila */
+    fwrite( &g_max_id, sizeof(g_max_id), 1, g_file); // Jauno elementu skaitu saglabajam faila
+    debug("G_MAX_ID: %d", g_max_id);
     
-    fwrite( & g_book_rec_pt[i], sizeof(book_rec_type), g_count, g_file);        
+    fwrite( g_book_rec_pt, sizeof(book_rec_type), g_count, g_file); // Ierakstam visus ierakstus no masīva failā    
     
-    fclose(g_file); /* Aizveram failu */
+    fclose(g_file); // Aizveram failu
 
     printline("Tava DB tika veiksmigi saglabata!");
 
@@ -313,9 +310,12 @@ int SaveDB(void) {
     return EXIT_SUCCESS;
 }
 
+/**
+    Procedūra izvadei ar newline galā
+*/
 int printline(const char *format, ...){
     va_list ap;
-    int v_return;
+    int v_return = EXIT_SUCCESS;
 
     va_start(ap, format);
     v_return = vprintf(format, ap);
@@ -324,20 +324,31 @@ int printline(const char *format, ...){
     return v_return;
 }
 
+/**
+    Programma debuggošanas procedūra
+
+    Lai strādātu, jāpadod -v --verbose parametrs palaišanas brīdī
+    ./library -v [--verbose]
+*/
 int debug(const char *format, ...){
     va_list ap;
+    FILE    *C_LOG_FILE = fopen("library.log", "a+"); // Debugošanas fails
+
     int v_return = EXIT_SUCCESS;
+
     if(C_DEBUG){
         va_start(ap,format);
         fprintf(C_LOG_FILE, "\n");
-        v_return = vfprintf(C_LOG_FILE, format, ap);        
-        vprintf(format, ap);
+        v_return = vfprintf(C_LOG_FILE, format, ap);                
         va_end(ap);
     }
 
     return v_return;
 }
 
+/**
+    Procedūre Atmiņas iedalīšanas kļūdas gadījumā
+*/
 int ERR_MEM_ALLOC( void ){
     char    ERR_MSG[] = "ERROR while allocating space!";
 
@@ -346,8 +357,12 @@ int ERR_MEM_ALLOC( void ){
     return EXIT_FAILURE;
 }
 
-int askForConfirmation(){
-    int v_return = 0;
+/**
+    Pieprasam apstiprinājumu
+    Atgriež 1, ja Y vai y, atgriež -1, ja N vai n
+*/
+int askForConfirmation( void ){
+    int v_return = EXIT_SUCCESS;
     char v_yes_no;
 
     while(v_return == 0){
@@ -365,22 +380,32 @@ int askForConfirmation(){
 
 int removeRecord(int indexToRemove)
 {        
-    memmove(&g_book_rec_pt[indexToRemove], &g_book_rec_pt[indexToRemove+1], (g_count - indexToRemove - 1) * sizeof(book_rec_type)); //Bīdam atmiņas blokus
-    g_count--;
-    g_book_rec_pt = (book_rec_type *) realloc(g_book_rec_pt,g_count* sizeof(struct book_rec_type));
+    memmove(&g_book_rec_pt[indexToRemove], &g_book_rec_pt[indexToRemove+1], (g_count - indexToRemove - 1) * sizeof(book_rec_type)); //Bīdam atmiņas blokus, lai "dzēstu"
+    g_count--; //Globālo ierakstu skaitītāju dekrementējam
+    //g_book_rec_pt = (book_rec_type *) realloc(g_book_rec_pt,g_count* sizeof(struct book_rec_type));
 
     if ( ( g_book_rec_pt == NULL )) {
-        return ERR_MEM_ALLOC();
+        exit(ERR_MEM_ALLOC());
     }
 
     return EXIT_SUCCESS;
 }
 
+/**
+    Atgriežam -1, ja funkcija ir beigusi savu darbību
+*/
 int searchElement( size_t offset, /*size_t memb_size,*/ int *target, const char* p_type ){
-    static int rec_nr = 0;
-    static int v_found = 0;
+    static int rec_nr = 0; //Elementa indexa skaitītājs
+    static int v_found = 0; //Atrasto ierakstu skaitītājs
 
     for (; rec_nr < g_count; rec_nr++) { // Tiek mekletas visas izdevniecibas
+        //Veicam vērtību salīdzīnāšanu izmantojot pointeru aritmētiku un tipu kāstošanu
+        /**
+            Padotā parametra konstante:
+                "CHAR" nozīmē, ka pārbaudāmais elements tiks kāstots kā pointeris uz "char" masīvu
+                "INT" nozīmē, ka pārbaudāmais elements tiks kāstots kā "int"
+                "USINT" nozīmē, ka pārbaudāmais elements tiks kāstots kā "unsigned short int"
+        */
         if (
                 (p_type == "CHAR" && strcmp(((char*)target), ((char*)((book_rec_type *)&g_book_rec_pt[rec_nr])+offset)) == 0)
                 ||
@@ -398,19 +423,22 @@ int searchElement( size_t offset, /*size_t memb_size,*/ int *target, const char*
             return rec_nr++;
         }
     }
-
+    //Neviens ieraksts netika atrasts
     if(v_found == 0){
         printline("The record could not be found!");
     }
-    else{
-        if(v_found == 1)
-            printline("One record totally found.");
-        else
+    //Atrasts tika vismaz 1 ieraksts
+    else
+    {        
+        if(v_found == 1) //Ja atrasts tikai viens ieraksts
+            printline("One record totally found.");        
+        else //Atrasti vairāki ieraksti
             printline("%d record totally found.", v_found);
-        v_found = 0;
+
+        v_found = 0; //Resetot ierakstu skaitītāju
     }
 
-    rec_nr = 0;
+    rec_nr = 0; //Resetot ierakstu skaitītāju
     
     return -1;
 }
